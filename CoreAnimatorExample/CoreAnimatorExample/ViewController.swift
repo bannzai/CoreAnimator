2c8f18ea573d8fa9ff79d785e2e12201a3b7e41e
bannzai <kingkong999yhirose@gmail.com>
2017-08-19 16:53:50 +0900

    :tada: Improved sequence animator

--- CoreAnimatorExample/CoreAnimatorExample/ViewController.swift
+++ CoreAnimatorExample/CoreAnimatorExample/ViewController.swift
@@ -11,16 +11,44 @@ import CoreAnimator
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        CoreAnimator()
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
     }
-
+    
+    func tapped() {
+        redView.alpha = 0
+        
+        SequenceAnimator()
+            .add(duration: 1, view: redView) {
+                $0.alpha = 1
+            }
+            .add(duration: 2, view: blueView) {
+                $0.frame.origin = .zero
+            }
+            .add(
+                duration: 1,
+                layer: yellowView.layer,
+                options: .easeInOut,
+                keyPath: KeyPathValue.cornerRadius.self,
+                fromValue: 0,
+                toValue: 20
+            )
+            .addCompletion {
+                print("end")
+            }
+            .animate()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    
 }
 
