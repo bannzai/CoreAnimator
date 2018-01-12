7f738adbd482b6fb528de3a9d62868f743594307
bannzai <kingkong999yhirose@gmail.com>
2018-01-13 01:44:58 +0900

    :recycle: remake keypathtype to enum

--- CoreAnimator/CoreAnimationKeyPathValue.swift
+++ CoreAnimator/CoreAnimationKeyPathValue.swift
@@ -16,6 +16,94 @@ public protocol ValueType {
     static var key: String { get }
 }
 
+public protocol KeyPath {
+    var key: String { get }
+    var value: Any { get }
+}
+
+public enum KeyPathType {
+    case cornerRadius(CGFloat)
+    case transform(CGAffineTransform)
+    case frame(CGRect)
+
+    public enum Frame {
+        case origin(CGPoint)
+        
+        public enum Origin {
+            case x(CGFloat)
+            case y(CGFloat)
+        }
+    }
+}
+
+extension KeyPathType: KeyPath {
+    public var key: String {
+        switch self {
+        case .cornerRadius:
+            return "cornerRadius"
+        case .transform:
+            return "transform"
+        case .frame:
+            return "frame"
+        }
+    }
+    
+    public var value: Any {
+        switch self {
+        case .cornerRadius(let cornerRadius):
+            return NSNumber(value: Float(cornerRadius))
+        case .transform(let value):
+            return NSValue(cgAffineTransform: input)
+        case .frame(let rect):
+            return NSValue(cgRect: rect)
+        }
+    }
+}
+
+extension KeyPathType.Frame: KeyPath {
+    var prefix: String {
+        return "frame"
+    }
+    
+    public var key: String {
+        switch self {
+        case .origin:
+            return "\(prefix).origin"
+        }
+    }
+    
+    public var value: Any {
+        switch self {
+        case .origin(let point):
+            return NSValue(cgPoint: point)
+        }
+    }
+}
+
+extension KeyPathType.Frame.Origin: KeyPath {
+    var prefix: String {
+        return "frame.origin"
+    }
+    
+    public var key: String {
+        switch self {
+        case .x:
+            return "x"
+        case .y:
+            return "y"
+        }
+    }
+    
+    public var value: Any {
+        switch self {
+        case .x(let x):
+            return NSNumber(value: Float(x))
+        case .y(let y):
+            return NSNumber(value: Float(y))
+        }
+    }
+}
+
 public struct KeyPathValueType {
     
     public struct cornerRadius: ValueType {
